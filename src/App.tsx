067e52a01781059e766efd4298fbd574a06a2469
oikawa <oikawa@MasakinoMacBook-Pro.local>
2024-03-24 17:21:05 +0900

    mod huskey

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import TextInput from "./components/Input/TextInput";
+
+function App() {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <TextInput />
+    </>
+  );
+}
+
+export default App;
